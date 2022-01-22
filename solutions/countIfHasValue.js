04da31d4f044e5e029ce3bcabf09393bafc945b5
stocktons <sarah@thanksforallthe.fish>
2022-01-21 20:11:44 -0500

    removed params from findFirstMove, clarified instructions on countIfHasValue

--- solutions/countIfHasValue.js
+++ solutions/countIfHasValue.js
@@ -1,6 +1,8 @@
 function countIfHasValue(obj, value) {
   /**
-   * Write a called countIfHasValue which accepts an object where all of the values are arrays. The function should count the number of times the value is found in any of the arrays.
+   * Write a function called countIfHasValue which accepts an object where all 
+   * of the values are arrays. The function should return the number of arrays
+   * in which the value appears.
    */
   let count = 0;
   for (let key in obj) {
